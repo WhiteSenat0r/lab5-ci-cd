be5144ecba189b8c825004ad1f380b924e8bcf1f
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2025-04-27 17:05:24 +0300

    щзх

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: ["./src/setupTests.ts"],
-  },
+  base: "/lab5-ci-cd/",
 });
