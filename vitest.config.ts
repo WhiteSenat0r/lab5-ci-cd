62bf310e960907c4b8571665e226b02e7cc76fc6
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2025-04-27 17:31:06 +0300

    idk whats happening

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/lab5-ci-cd/",
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["./src/test/setup.ts"], // Optional: for any global test setup
+  },
 });
